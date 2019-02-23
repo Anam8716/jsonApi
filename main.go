c31fb8e9f686649a8fcc0577e004434974f56c1c
Anam <anamali8716@gmail.com>
2019-02-23 18:49:20 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// User struct (Model)
+type User struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	DOB    string  `json:"title"`
+	Friend *Friend `json:"friend"`
+}
+
+// Author struct
+type Friend struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	DOB  string `json:"title"`
+}
+
+// Init users var as a slice user struct
+var users []User
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Gets params
+	// Loop through users and find one with the id from the params
+	for _, item := range users {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&User{})
+}
+
+// Add new user
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	user.ID = strconv.Itoa(rand.Intn(100000000)) // Mock ID - not safe
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
+}
+
+// Update user
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range users {
+		if item.ID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			var user User
+			_ = json.NewDecoder(r.Body).Decode(&user)
+			user.ID = params["id"]
+			users = append(users, user)
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+}
+
+// Delete user
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range users {
+		if item.ID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+// Main function
+func main() {
+	// Init router
+	r := mux.NewRouter()
+
+	// Hardcoded data - @todo: add database
+	users = append(users, User{ID: "1", Name: "anam", DOB: "5 sep", Friend: &Friend{ID: "f1", Name: "f1", DOB: "5 july"}})
+
+	// Route handles & endpoints
+	r.HandleFunc("/users", getUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", getUser).Methods("GET")
+	r.HandleFunc("/users", createUser).Methods("POST")
+	r.HandleFunc("/users/{id}", updateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+
+	// Start server
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
